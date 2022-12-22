63f5b9ca63b6eaef6760dde84967f84fd262cdf8
ouasamine <ouas.amine@outlook.com>
2022-12-22 11:22:18 +0100

    Create CapitalizeDecorator class

--- decorators.rb
+++ decorators.rb
@@ -3,3 +3,20 @@ class Nameable
     raise NotImplementedError;
   end
 end
+
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
