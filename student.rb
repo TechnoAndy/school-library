c2ce1b8e9271891334d40c1cf18dbed29fd3ea75
ouasamine <ouas.amine@outlook.com>
2022-12-22 23:08:51 +0100

    Create relationship between Classroom and Student classes

--- student.rb
+++ student.rb
@@ -4,6 +4,7 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    @classroom.students << self
   end
 
   def play_hooky
