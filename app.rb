1c40883da3b21e35b1558a3828025a017e926d0d
ouasamine <ouas.amine@outlook.com>
2022-12-23 20:19:57 +0100

    Implement list_books method in app.rb

--- app.rb
+++ app.rb
@@ -1,15 +1,27 @@
-require './person'
-require './classroom'
+require './student'
 require './book'
 require './rental'
 
 class App
+  def initialize
+    @books = []
+    @teachers = []
+    @students = []
+  end
+
+  def list_books
+    @books.map { |book, index| puts "#{index} Title: #{book.title}   Author: #{book.author}" }
+  end
+
   def create_person(age, name)
-    person = Person.new(age, name)
+    person = Student.new(age, name)
+    @students << person
   end
 
   def create_book(title, author)
     book = Book.new(title, author)
+    @books << book
   end
 
+  
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require './app'
 
 def main
-  welcome_message = "Welcome to School Library App! \n\n 
-  Please choose an option to proceed: \n 
+  welcome_message = "\nWelcome to School Library App! \n\n 
+  Please choose an option to proceed:\n
   1 - List all books
   2 - List all people
   3 - Create a person
@@ -10,20 +10,19 @@ def main
   5 - Create a rental
   6 - List all rentals for a given person id
   7 - Exit "
-  
+  app = App.new
 
-  def launch_app(message)
-    app = App.new
+  def launch_app(message, app)
     puts message
     input = gets.chomp
 
     case input
     when "1"
-      print "cool!"
-      launch_app(message)
+      app.list_books
+      launch_app(message, app)
     when "2"
       print "cool!"
-      launch_app(message)
+      launch_app(message, app)
     when "3"
       print "Enter the person name: " 
       name_input = gets.chomp
@@ -31,7 +30,7 @@ def main
       age_input = gets.chomp
       app.create_person(age_input, name_input)
       puts "Person Created Successfully"
-      launch_app(message)
+      launch_app(message, app)
     when "4"
       print "Enter the book's title: " 
       btitle_input = gets.chomp
@@ -39,22 +38,27 @@ def main
       bauthor_input = gets.chomp
       app.create_book(btitle_input, bauthor_input)
       puts "Book Created Successfully"
-      launch_app(message)
+      launch_app(message, app)
     when "5"
-      print "cool!"
-      launch_app(message)
+      print "Enter the rented book's title: " 
+      btitle_input = gets.chomp
+      print "Enter the book's author: " 
+      bauthor_input = gets.chomp
+      app.create_book(btitle_input, bauthor_input)
+      puts "Book Created Successfully"
+      launch_app(message, app)
     when "6"
       print "cool!"
-      launch_app(message)
+      launch_app(message, app)
     when "7"
       puts "Exiting...."
       exit
     else
       puts "Please Enter a Valid option"
-      launch_app(message)
+      launch_app(message, app)
     end
   end
-  launch_app(welcome_message)
+  launch_app(welcome_message, app)
   
 end
 
