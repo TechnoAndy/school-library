2894ed321454a5e87b1d3c307dc53ba72f54ca05
ouasamine <ouas.amine@outlook.com>
2022-12-23 23:48:07 +0100

    Replace main method with Main class

--- main.rb
+++ main.rb
@@ -1,86 +1,96 @@
 require './app'
 
-def launch_app(message, app)
-  puts message
-  input = gets.chomp
+class Main
+  def initialize(message, app)
+    @message = message
+    @app = app
+  end
 
-  case input
-  when '1'
-    app.list_books
-    launch_app(message, app)
-  when '2'
-    app.list_people
-    launch_app(message, app)
-  when '3'
-    print 'Do you want to create a (1) student OR a (2) teacher: '
-    type_input = gets.chomp
-    print 'Enter the person name: '
-    name_input = gets.chomp
-    print 'Enter the person age: '
-    age_input = gets.chomp
-
-    case type_input
-    when '1'
-      print "Does the student have their parent's permission? [Y/N] "
-      permission_input = gets.chomp.upcase
-
-      case permission_input
-      when 'Y'
-        app.create_person(age_input, name_input, type_input, true)
-      when 'N'
-        app.create_person(age_input, name_input, type_input, false)
-      else
-        puts 'Please Enter a Valid option'
-        exit
-      end
-
-    when '2'
-      print 'What is the specialization of the teacher? '
-      spec_input = gets.chomp
-      app.create_person(age_input, name_input, type_input, nil, spec_input)
+  def check_permission_input(permission_input)
+    case permission_input
+    when 'Y'
+      return true
+    when 'N'
+      return false
     else
       puts 'Please Enter a Valid option'
-      launch_app(message, app)
+      exit
     end
-
-    puts 'Person Created Successfully'
-    launch_app(message, app)
-  when '4'
-    print "Enter the book's title: "
-    btitle_input = gets.chomp
-    print "Enter the book's author: "
-    bauthor_input = gets.chomp
-    app.create_book(btitle_input, bauthor_input)
-    puts 'Book Created Successfully'
-    launch_app(message, app)
-  when '5'
-    app.list_books
-    print 'Please pick one of the available books: '
-    input_book_index = gets.chomp
-    app.list_people
-    print 'Please define the person renting the book: '
-    input_renter_index = gets.chomp
-    print 'Please enter the rental date [yyyy-mm-dd]:  '
-    input_date = gets.chomp
-    app.create_rental(input_date, input_book_index, input_renter_index)
-    launch_app(message, app)
-  when '6'
-    # app.list_people
-    print 'Enter the ID of the person: '
-    input_id = gets.chomp
-    app.list_rentals(input_id.to_i)
-    launch_app(message, app)
-  when '7'
-    puts 'Exiting....'
-    exit
-  else
-    puts 'Please Enter a Valid option'
-    launch_app(message, app)
   end
+
+  def launch
+    puts @message
+    input = gets.chomp
+
+    case input
+    when '1'
+      @app.list_books
+      launch
+    when '2'
+      @app.list_people
+      launch
+    when '3'
+      print 'Do you want to create a (1) student OR a (2) teacher: '
+      type_input = gets.chomp
+      print 'Enter the person name: '
+      name_input = gets.chomp
+      print 'Enter the person age: '
+      age_input = gets.chomp
+  
+      case type_input
+      when '1'
+        print "Does the student have their parent's permission? [Y/N] "
+        permission_input = gets.chomp.upcase
+        permission = check_permission_input(permission_input)
+        @app.create_person(age_input, name_input, type_input, permission)
+      when '2'
+        print 'What is the specialization of the teacher? '
+        spec_input = gets.chomp
+        @app.create_person(age_input, name_input, type_input, nil, spec_input)
+      else
+        puts 'Please Enter a Valid option'
+        launch
+      end
+  
+      puts 'Person Created Successfully'
+      launch
+    when '4'
+      print "Enter the book's title: "
+      btitle_input = gets.chomp
+      print "Enter the book's author: "
+      bauthor_input = gets.chomp
+      @app.create_book(btitle_input, bauthor_input)
+      puts 'Book Created Successfully'
+      launch
+    when '5'
+      @app.list_books
+      print 'Please pick one of the available books: '
+      input_book_index = gets.chomp
+      @app.list_people
+      print 'Please define the person renting the book: '
+      input_renter_index = gets.chomp
+      print 'Please enter the rental date [yyyy-mm-dd]:  '
+      input_date = gets.chomp
+      @app.create_rental(input_date, input_book_index, input_renter_index)
+      launch
+    when '6'
+      print 'Enter the ID of the person: '
+      input_id = gets.chomp
+      @app.list_rentals(input_id.to_i)
+      launch
+    when '7'
+      puts 'Exiting....'
+      exit
+    else
+      puts 'Please Enter a Valid option'
+      launch
+    end
+  end
+
 end
 
 def main
-  welcome_message = "\nWelcome to School Library App! \n\n
+  welcome_message = "\n################################\nWelcome to School Library App! \n\n
   Please choose an option to proceed:\n
   1 - List all books
   2 - List all people
@@ -91,7 +101,7 @@ def main
   7 - Exit "
   app = App.new
 
-  launch_app(welcome_message, app)
+  Main.new(welcome_message, app).launch
 end
 
 main
